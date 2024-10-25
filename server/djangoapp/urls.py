75b1d353b407433bfa137f59afa0c1176d3fd876
jsswp2004 <jsswp2004@outlook.com>
2024-10-24 20:48:51 -0400

    updates 10/24

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     # path for logout
     path(route='logout', view=views.logout_request, name='logout'),
     # path for get dealerships
-    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
     path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
     # path for get dealer details
     path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
