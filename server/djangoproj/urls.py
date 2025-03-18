db48a8f92eab97782b1356c96cd48721aced8f4a
jsswp2004 <jsswp2004@outlook.com>
2025-03-18 00:30:34 -0400

    resolved issue with dealershit

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -29,9 +29,9 @@ urlpatterns = [
     path('about/', TemplateView.as_view(template_name="About.html")),
     path('contact/', TemplateView.as_view(template_name="Contact.html")),
     path('dealers/', TemplateView.as_view(template_name="index.html")),
-    path('dealer/', TemplateView.as_view(template_name="index.html")),
+    #path('dealer/', TemplateView.as_view(template_name="index.html")),
     path('dealer/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
     path('postreview/<int:dealer_id>/', add_review, name='add_review'),
-    path('djangoapp/add_review/', add_review, name='add_review'),
+    #path('djangoapp/add_review/', add_review, name='add_review'),
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
