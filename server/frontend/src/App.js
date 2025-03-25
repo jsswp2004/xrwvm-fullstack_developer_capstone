69332c5528f54e419e647b2c100ff506765ac944
jsswp2004 <jsswp2004@outlook.com>
2025-03-24 22:35:26 -0400

    able to get dealership by moving static files

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,14 @@
-import LoginPanel from "./components/Login/Login"
+import LoginPanel from "./components/Login/Login";
+import Dealers from "./components/Dealers/Dealers";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
+      <Route path="/dealers" element={<Dealers />} />
     </Routes>
   );
 }
+
 export default App;
